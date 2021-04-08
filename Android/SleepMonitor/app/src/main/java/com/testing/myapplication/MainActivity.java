d69af0ee16ff20c085046194c63c5d9bf9c843f2
YuankaiZhu <52915611+YuankaiZhu@users.noreply.github.com>
2021-04-08 20:00:05 +0800

    Add Line Chart Example

--- Android/SleepMonitor/app/src/main/java/com/testing/myapplication/MainActivity.java
+++ Android/SleepMonitor/app/src/main/java/com/testing/myapplication/MainActivity.java
@@ -2,22 +2,66 @@ package com.testing.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 import org.json.JSONArray;
 import org.json.JSONException;
-import com.vishnusivadas.advanced_httpurlconnection.FetchData;
-import java.util.ArrayList;
-import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.vishnusivadas.advanced_httpurlconnection.FetchData;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+
+class LineChartXAxisValueFormatter extends IndexAxisValueFormatter {
+
+    @Override
+    public String getFormattedValue(float value) {
+
+        // Convert float value to date string
+        // Convert from seconds back to milliseconds to format time  to show to the user
+        long emissionsMilliSince1970Time = ((long) value) * 1000;
+
+        // Show time in local version
+        Date timeMilliseconds = new Date(emissionsMilliSince1970Time);
+//        DateFormat dateTimeFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault());
+        DateFormat dateTimeFormat = DateFormat.getDateTimeInstance();
+
+        return dateTimeFormat.format(timeMilliseconds);
+    }
+}
+
+public class MainActivity extends AppCompatActivity implements OnChartGestureListener, OnChartValueSelectedListener {
     Button ButtonFetchDataTest;
     ProgressBar progressBar;
+    private static final String TAG = "MainActivity";
+    private LineChart mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +70,74 @@ public class MainActivity extends AppCompatActivity {
 
         ButtonFetchDataTest = findViewById(R.id.ButtonFetchDataTest);
         progressBar = findViewById(R.id.progress);
+
+
+
+        mChart = (LineChart) findViewById(R.id.tempLineChart);
+
+        mChart.setOnChartGestureListener(MainActivity.this);
+        mChart.setOnChartValueSelectedListener(MainActivity.this);
+
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+
+        LimitLine upper_limit = new LimitLine(65f,"Danger");
+        upper_limit.setLineWidth(4f);
+        upper_limit.enableDashedLine(10f,10f,10f);
+        upper_limit.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_TOP);
+        upper_limit.setTextSize(15f);
+
+
+        LimitLine lower_limit = new LimitLine(35f,"Too Low");
+        lower_limit.setLineWidth(4f);
+        lower_limit.enableDashedLine(10f,10f,10f);
+        lower_limit.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+        lower_limit.setTextSize(15f);
+
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        leftAxis.removeAllLimitLines();
+        leftAxis.addLimitLine(upper_limit);
+        leftAxis.addLimitLine(lower_limit);
+        leftAxis.setAxisMaximum(100f);
+        leftAxis.setAxisMinimum(25f);
+        leftAxis.enableGridDashedLine(10f,10f,0);
+        leftAxis.setDrawLimitLinesBehindData(true);
+
+
+        mChart.getAxisRight().setEnabled(false);
+
+        ArrayList<Entry> yValues = new ArrayList<>();
+
+
+        yValues.add(new Entry(0,60f));
+        yValues.add(new Entry(100,50f));
+        yValues.add(new Entry(200,70f));
+        yValues.add(new Entry(300,30f));
+        yValues.add(new Entry(400,50f));
+        yValues.add(new Entry(500,60f));
+        yValues.add(new Entry(600,65f));
+
+        LineDataSet set1 = new LineDataSet(yValues,"Data set 1");
+        set1.setFillAlpha(110);
+        set1.setColor(Color.RED);
+        set1.setLineWidth(3f);
+        set1.setValueTextSize(10f);
+        set1.setValueTextColor(Color.BLACK);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+        LineData data = new LineData(dataSets);
+        mChart.setData(data);
+
+
+//        String[] values = new String[]{"Jan","Feb","Mar","Apr","May","June","July"};
+
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setValueFormatter(new LineChartXAxisValueFormatter());
+        xAxis.setLabelRotationAngle(-90);
+       // xAxis.setGranularity(1);
+
 
         ButtonFetchDataTest.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,4 +182,66 @@ public class MainActivity extends AppCompatActivity {
                 }
         });
     }
+
+    @Override
+    public void onChartGestureStart(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+
+    }
+
+    @Override
+    public void onChartGestureEnd(MotionEvent me, ChartTouchListener.ChartGesture lastPerformedGesture) {
+
+    }
+
+    @Override
+    public void onChartLongPressed(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartDoubleTapped(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartSingleTapped(MotionEvent me) {
+
+    }
+
+    @Override
+    public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
+
+    }
+
+    @Override
+    public void onChartScale(MotionEvent me, float scaleX, float scaleY) {
+
+    }
+
+    @Override
+    public void onChartTranslate(MotionEvent me, float dX, float dY) {
+
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+
+    }
+
+    @Override
+    public void onNothingSelected() {
+
+    }
 }
+
+//class MyXAxisValueFormatter extends ValueFormatter implements IAxisValueFormatter {
+//    private String[] mValues;
+//    public MyXAxisValueFormatter(String[] values){
+//        this.mValues = values;
+//    }
+//
+//    @Override
+//    public String getFormattedValue(float value, AxisBase axis){
+//        return mValues[(int)value];
+//    }
+//}
