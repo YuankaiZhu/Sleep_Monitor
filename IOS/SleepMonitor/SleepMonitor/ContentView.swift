3c877e43efc905e71e9f0312a372d0ca8aadaa11
YuankaiZhu <52915611+YuankaiZhu@users.noreply.github.com>
2021-04-09 02:14:17 -0700

    IOS App shows motion & temperature

--- IOS/SleepMonitor/SleepMonitor/ContentView.swift
+++ IOS/SleepMonitor/SleepMonitor/ContentView.swift
@@ -11,24 +11,55 @@ struct ContentView: View {
     @State var mpu6050: [MPU6050] = []
     var body: some View {
         VStack{
-            List(mpu6050) { user in
-
-                Text(user.time ?? "")
-                        .font(.headline)
-                Text(user.temp ?? "")
-                        .font(.subheadline)
-            }
-                .onAppear {
-                    FetchMPU6050().getUsers { (mpu6050) in
-                        self.mpu6050 = mpu6050
-                    }
-                }
-            let temps:[String?] = mpu6050.map{$0.temp}
-            let doubles = temps.compactMap{ Double($0 ?? "25") }
+//            List(mpu6050) { user in
+//
+//                Text(user.time ?? "")
+//                        .font(.headline)
+//                Text(user.temp ?? "")
+//                        .font(.subheadline)
+//            }
+//                .onAppear {
+//                    FetchMPU6050().getUsers { (mpu6050) in
+//                        self.mpu6050 = mpu6050
+//                    }
+//                }
+            let tempsStr:[String?] = mpu6050.map{$0.temp}
+            let temps = tempsStr.compactMap{ Double($0 ?? "25") }
             //[8.2,23.4,54,32,12,37,7,23,43]
-            LineView(data:Array(doubles.prefix(3000)), title: "Line chart", legend: "Full screen").padding()
-            // legend is optional, use optional .padding()
+            let accelXStr:[String?] = mpu6050.map{$0.accel_x}
+            let accelX = accelXStr.compactMap{ Double($0 ?? "0") }.map(abs)
+            let accelYStr:[String?] = mpu6050.map{$0.accel_y}
+            let accelY = accelYStr.compactMap{ Double($0 ?? "0") }.map(abs)
+            let accelZStr:[String?] = mpu6050.map{$0.accel_z}
+            let accelZ = accelZStr.compactMap{ Double($0 ?? "0") }.map(abs)
+            let gyroXStr:[String?] = mpu6050.map{$0.gyro_x}
+            let gyroX = gyroXStr.compactMap{ Double($0 ?? "0") }.map(abs)
+            let gyroYStr:[String?] = mpu6050.map{$0.gyro_y}
+            let gyroY = gyroYStr.compactMap{ Double($0 ?? "0") }.map(abs)
+            let gyroZStr:[String?] = mpu6050.map{$0.gyro_z}
+            let gyroZ = gyroZStr.compactMap{ Double($0 ?? "0") }.map(abs)
+
+            let motionAccel = zip(zip(accelX,accelY).map(+),accelZ).map(+)
+            let motionGyro = zip(zip(gyroX,gyroY).map(+),gyroZ).map(+)
+            let motion = zip(motionAccel,motionGyro).map(+)
+            
+            LineView(data:Array(motion), title: "Sleep Motion").padding()
+            LineView(data:Array(temps), title: "Temperature", legend: "Centigrade").padding()
+            
+            VStack{
+                Text("Maximum Temperature: \(temps.max() ?? 0) Centigrade")
+                Text("Minimum Temperature: \(temps.min() ?? 0) Centigrade")
+                let aveTemp = temps.average
+                Text("Average Temperature: \(aveTemp) Centigrade")
+                
+            }
+            
         }
+        .onAppear {
+                            FetchMPU6050().getUsers { (mpu6050) in
+                                self.mpu6050 = mpu6050
+                            }
+                        }
 }
 }
 
@@ -36,4 +67,20 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+extension Array where Element: FloatingPoint {
+    
+    var sum: Element {
+        return reduce(0, +)
+    }
+
+    var average: Element {
+        guard !isEmpty else {
+            return 0
+        }
+        return sum / Element(count)
+    }
+
 }
--- IOS/SleepMonitor/SleepMonitor/MPU6050.swift
+++ IOS/SleepMonitor/SleepMonitor/MPU6050.swift
@@ -9,5 +9,11 @@ import Foundation
 struct MPU6050: Codable, Identifiable {
     let id = UUID()
     let time: String?
+    let accel_x:String?
+    let accel_y:String?
+    let accel_z:String?
+    let gyro_x:String?
+    let gyro_y:String?
+    let gyro_z:String?
     let temp: String?
 }
